2d83468e5885b1484e59e8079c65b4f3ff231173
Fabien Carré <carre.fabien@gmail.com>
2024-06-17 08:19:09 +0200

    [debug]

--- install.sh
+++ install.sh
@@ -8,13 +8,15 @@ read -n 1 -r -s -p $'    Press any key to continue or Ctrl+C to abort...\n\n'
 
 # Install Homebrew
 command -v brew >/dev/null 2>&1 ||
-    (echo '🍺  Installing Homebrew' && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)")
+    (echo '🍺  Installing Homebrew' && /bin/bash -c "$(git clone https://github.com/Homebrew/brew ${HOME}.homebrew)")
+
+export PATH=${PATH}:${HOME}/.homebrew/bin
 
 # Install chezmoi
 command -v chezmoi >/dev/null 2>&1 ||
     (echo '👊  Installing chezmoi' && brew install chezmoi)
 
-if [ -d "$HOME/.local/share/chezmoi/.git" ]; then
+if [ -d "${HOME}/.local/share/chezmoi/.git" ]; then
     echo "🚸  chezmoi already initialized"
     echo "    Reinitialize with: 'chezmoi init https://github.com/igor012/dotfiles.git'"
 else
