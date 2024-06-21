1feba5cd28dbf8032dd243453fff6333ef187c68
Fabien Carré <fabien.carre@passculture.app>
2024-06-21 23:55:31 +0200

    [UPD] nvim

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -61,3 +61,17 @@ vim.cmd('Plug \'nvim-pack/nvim-spectre\'')
 vim.cmd('call plug#end()')
 
 vim.cmd('colorscheme dracula')
+
+
+-- local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+-- if not (vim.uv or vim.loop).fs_stat(lazypath) then
+--   vim.fn.system({
+--     "git",
+--     "clone",
+--     "--filter=blob:none",
+--     "https://github.com/folke/lazy.nvim.git",
+--     "--branch=stable", -- latest stable release
+--     lazypath,
+--   })
+-- end
+-- vim.opt.rtp:prepend(lazypath)
